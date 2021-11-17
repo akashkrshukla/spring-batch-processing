669c560903f284983dfcfa297ae82374606e7c8e
Akash Shukla <akashshukla778@gmail.com>
2021-11-17 02:43:09 -0500

    Lab 7 completion

---
+++ src/main/java/com/nexttech/batchprocessing/NextTechBatchProcessingApplication.java
@@ -0,0 +1,11 @@
+package com.nexttech.batchprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextTechBatchProcessingApplication {
+	public static void main(String[] args) throws Exception {
+		System.exit(SpringApplication.exit(SpringApplication.run(NextTechBatchProcessingApplication.class, args)));
+	}
+}
